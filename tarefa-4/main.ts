c12e7821ac9c3fab9eafb2bbe119f7da1e26b522
Elany Peixoto <elanysantos5@outlook.com>
2024-10-31 18:54:47 +0000

    feat: adiciona principio da segregração

---
+++ tarefa-4/main.ts
@@ -0,0 +1,63 @@
+interface Pagamento {
+    valor: number;
+    pagar(): void;
+}
+
+class PagamentoCheque implements Pagamento {
+    valor: number;
+
+    constructor(valor: number) {
+        this.valor = valor;
+    }
+    pagar(): void {
+        console.log(`O pagamento de ${this.valor} será feito por cheque.`);
+    }
+}
+
+class PagamentoDinheiro implements Pagamento {
+    valor: number;
+
+    constructor(valor: number) {
+        this.valor = valor;
+    }
+
+    pagar(): void {
+        console.log(`O pagamento de ${this.valor} será feito por dinheiro.`);
+    }
+}
+
+class PagamentoPorPix implements Pagamento {
+    valor: number;
+
+    constructor(valor: number) {
+        this.valor = valor;
+    }
+
+    pagar(): void {
+        console.log(`O pagamento de ${this.valor} será feito por pix.`);
+    }
+}
+
+class PagamentoPorBoleto implements Pagamento {
+    valor: number;
+
+    constructor(valor: number) {
+        this.valor = valor;
+    }
+
+    pagar(): void {
+        console.log(`O pagamento de ${this.valor} será feito por Boleto.`);
+    }
+}
+
+const pagamentoCheque = new PagamentoCheque(100);
+pagamentoCheque.pagar();
+
+const pagamentoDinheiro = new PagamentoDinheiro(200);
+pagamentoDinheiro.pagar();
+
+const pagamentoTransferencia = new PagamentoPorPix(300);
+pagamentoTransferencia.pagar();
+
+const pagamentoBoleto = new PagamentoPorBoleto(300);
+pagamentoBoleto.pagar();
